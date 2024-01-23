018bca7bfd9e49fcd1a0805a07f1a7c72869fd70
StefanE85a <>
2024-01-23 10:46:30 +0000

    Added user input section

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Import random package to be able to create random integers
 import random
 
+
 # The Game Board
 def print_board(board):
     for row in board:
@@ -13,3 +14,12 @@ def random_row(board):
 
 def random_col(board):
     return random.randint(0, len(board[0]) - 1)
+
+
+# User input
+def get_user_input(prompt):
+    try:
+        return int(input(prompt))
+    except ValueError:
+        print("Please enter a number.")
+        return get_user_input(prompt)
