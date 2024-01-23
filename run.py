6f909210e17198b2fbe581ba7a7406333fb74595
StefanE85a <>
2024-01-23 10:44:12 +0000

    Added section for game board

--- run.py
+++ run.py
@@ -1,2 +1,15 @@
 # Import random package to be able to create random integers
 import random
+
+# The Game Board
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+
+def random_row(board):
+    return random.randint(0, len(board) - 1)
+
+
+def random_col(board):
+    return random.randint(0, len(board[0]) - 1)
